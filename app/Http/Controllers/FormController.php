bdfad81c3b6bc65fdbb285e642eea5add1aa8dd7
amir <amirjoun2@gmail.com>
2025-03-23 18:47:02 +0400

    Added Controller for Form

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductForm;
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+    public function __construct(public ProductForm $productForm)
+    {
+    }
+    public function index()
+    {
+        $form = $this->productForm->getProductForm();
+
+        $this->productForm->loadProductData($form);
+        
+        echo $this->productForm->renderProductForm($form);
+    }
+}
